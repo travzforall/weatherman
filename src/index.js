1a92e218fb7152e8694d24dceedccf9593b9a8af
Travon Campbell <travonlcampbell@gmail.com>
2018-08-03 12:34:36 -0400

    TWM06 WIP

--- src/index.js
+++ src/index.js
@@ -1,20 +1,55 @@
 import "./scss/main.scss";
 // import 'jquery';
- 
+
+let locate_user;
+
 
 
 $(document).ready(() => {
+    locate_user = $('#locate-user');
+    let user_form, user;
+ 
+    user_form = $('#user-form');
+    user_form.submit(event => {
+        // event.preventDefault();
+        user = $('#user');
+        const validated = validateInput(user);
+
+        if (validated) {
+            console.log("Data sent to server, then JSON recieved")
+        } else {
+            const user = $("#user");
+            // $("#locate-user").tooltip('show');
+        }
+
+    }); 
+
     changeBackground();
 });
 
+
+
+
+
+
+function addEventListener(element, fn) { 
+    $(element).click(fn);
+}
+
+
+
 function changeBackground(type) {
     let body = $('body');
-    console.log(body);
+    console.log(locate_user)
     if (type === undefined) {
- 
- 
+
+
     }
+}
 
-
-
+function validateInput(inputField) {
+    var sd = 4;
+    console.log(sd);
+    console.log($(inputField).val());
+    return false;
 }
